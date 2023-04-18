dca55f2474daf2a1a1455d4ef9e575add6a27037
Craig Burden <craigburdenmsc@gmail.com>
2023-04-18 19:11:53 +0100

    Swapped to servos

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,36 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include <EEPROM.h>
 
 #define SERIAL_BAUDRATE     115200
 
-#define PIN_SOLENOID1       1
-#define PIN_SOLENOID2       2
+#define PIN_SERVO1          32
+#define PIN_SERVO2          33
 
-#define RETRACTED           255
-#define EXTENDED            0
-#define PARTIALLY_EXTENDED  127
+#define SERVO_MIN_PULSE_US  100
+#define SERVO_MAX_PULSE_US  2000
+
+#define RETRACTED           0
+#define EXTENDED            180
+#define PARTIALLY_EXTENDED  90
+
+Servo servo1;
+Servo servo2;
 
 typedef struct 
 {
-  uint8_t solenoid1_position;
-  uint8_t solenoid2_position;
+  uint32_t servo1_position;
+  uint32_t servo2_position;
   uint32_t state_delay;
 } state_t;
 
 state_t latch_test_sequence[] =
 {
-  (state_t){.solenoid1_position = RETRACTED,  .solenoid2_position = EXTENDED,           .state_delay = 1000},
-  (state_t){.solenoid1_position = RETRACTED,  .solenoid2_position = RETRACTED,          .state_delay = 1000},
-  (state_t){.solenoid1_position = RETRACTED,  .solenoid2_position = PARTIALLY_EXTENDED, .state_delay = 1000},
-  (state_t){.solenoid1_position = EXTENDED,   .solenoid2_position = RETRACTED,          .state_delay = 1000},
-  (state_t){.solenoid1_position = RETRACTED,  .solenoid2_position = EXTENDED,           .state_delay = 1000},
+  (state_t){.servo1_position = RETRACTED,  .servo2_position = EXTENDED,           .state_delay = 1000},
+  (state_t){.servo1_position = RETRACTED,  .servo2_position = RETRACTED,          .state_delay = 1000},
+  (state_t){.servo1_position = RETRACTED,  .servo2_position = PARTIALLY_EXTENDED, .state_delay = 1000},
+  (state_t){.servo1_position = EXTENDED,   .servo2_position = RETRACTED,          .state_delay = 1000},
+  (state_t){.servo1_position = RETRACTED,  .servo2_position = EXTENDED,           .state_delay = 1000},
 };
 
 int iteration_counter = 1;
@@ -32,11 +40,8 @@ void setup()
   Serial.begin(SERIAL_BAUDRATE);
   Serial.println("--- Latch Tester ---");
 
-  pinMode(PIN_SOLENOID1, OUTPUT);
-  pinMode(PIN_SOLENOID2, OUTPUT);
-
-  analogWrite(PIN_SOLENOID1, EXTENDED);
-  analogWrite(PIN_SOLENOID2, EXTENDED);
+  servo1.attach(PIN_SERVO1);
+  servo2.attach(PIN_SERVO2);
 }
 
 void loop()
@@ -46,8 +51,8 @@ void loop()
 
   for (uint32_t sequence_index = 0; sequence_index < (sizeof(latch_test_sequence)/sizeof(state_t)); sequence_index++)
   {
-    analogWrite(PIN_SOLENOID1, latch_test_sequence[sequence_index].solenoid1_position);
-    analogWrite(PIN_SOLENOID2, latch_test_sequence[sequence_index].solenoid2_position);
+    servo1.write(latch_test_sequence[sequence_index].servo1_position);
+    servo2.write(latch_test_sequence[sequence_index].servo2_position);
     delay(latch_test_sequence[sequence_index].state_delay);
   }
 }
